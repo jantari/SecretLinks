d2cc566867d325e4f3121adda1bc97449d99ed05
jantari <jantari@outlook.de>
2024-01-16 01:03:48 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module secretlinks
+
+go 1.21.5
+
+require (
+	github.com/go-chi/chi/v5 v5.0.11
+	github.com/google/uuid v1.5.0
+)
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+    "fmt"
+
+    "html/template"
+    "net/http"
+    "encoding/json"
+
+    "github.com/go-chi/chi/v5"
+    "github.com/google/uuid"
+)
+
+type Secret struct {
+    Secret string `json:"secret"`
+    Views  int    `json:"views"`
+}
+
+var SecretStore = make(map[uuid.UUID]Secret)
+
+func main() {
+    fmt.Println("KEK")
+
+    tmpl := template.Must(template.ParseFiles("view.html"))
+
+    router := chi.NewRouter()
+    router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+        data := struct {
+            Secret string
+        }{
+            Secret: "SecretLinks",
+        }
+        tmpl.Execute(w, data)
+    })
+    router.Post("/secret", func(w http.ResponseWriter, r *http.Request) {
+        payload := &Secret{}
+        d := json.NewDecoder(r.Body)
+        d.DisallowUnknownFields() // catch unwanted fields
+
+        err := d.Decode(&payload)
+        if err != nil {
+            // bad JSON or unrecognized json field
+            http.Error(w, err.Error(), http.StatusBadRequest)
+            return
+        }
+
+        id := uuid.New()
+        if payload.Views < 1 {
+            payload.Views = 1
+        }
+        SecretStore[id] = *payload
+
+        w.Write([]byte(fmt.Sprintf("%v", id)))
+    })
+    router.Get("/secret/{id}", func(w http.ResponseWriter, r *http.Request) {
+        id := chi.URLParam(r, "id")
+        secretId, err := uuid.Parse(id)
+        if err != nil {
+            http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+            return
+        }
+        retrievedSecret, ok := SecretStore[secretId]
+        if ok && retrievedSecret.Views > 0 {
+            retrievedSecret.Views--
+            SecretStore[secretId] = retrievedSecret
+            tmpl.Execute(w, retrievedSecret)
+        } else {
+            http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+            return
+        }
+    })
+
+    http.ListenAndServe(":8080", router)
+}
