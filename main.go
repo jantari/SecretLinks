7fe877b61b282fad49948a016179c5fa16c6d1f2
jantari <jantari@outlook.de>
2024-01-23 23:11:14 +0100

    move newSecret API behind /api route-group

--- main.go
+++ main.go
@@ -19,83 +19,87 @@ type Secret struct {
 
 var SecretStore = make(map[uuid.UUID]Secret)
 
-func main() {
-    fmt.Println("KEK")
+var tmpl = template.Must(template.ParseFiles("view.html"))
+var clickthroughPage = template.Must(template.ParseFiles("reveal.html"))
 
-    tmpl := template.Must(template.ParseFiles("view.html"))
-    clickthroughPage := template.Must(template.ParseFiles("reveal.html"))
+func main() {
+    fmt.Println("SecretLinks")
 
     router := chi.NewRouter()
     router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        data := struct {
-            Secret string
-        }{
-            Secret: "SecretLinks",
-        }
-        tmpl.Execute(w, data)
+        w.Write([]byte("SecretLinks"))
     })
-    router.Post("/secret", func(w http.ResponseWriter, r *http.Request) {
-        payload := &Secret{}
-        d := json.NewDecoder(r.Body)
-        d.DisallowUnknownFields() // catch unwanted fields
-
-        err := d.Decode(&payload)
-        if err != nil {
-            // bad JSON or unrecognized json field
-            http.Error(w, err.Error(), http.StatusBadRequest)
-            return
-        }
-
-        id := uuid.New()
-        if payload.Views < 1 {
-            payload.Views = 1
-        }
-        SecretStore[id] = *payload
-
-        w.Write([]byte(fmt.Sprintf("%v", id)))
+    router.Route("/api", func(r chi.Router) {
+        router.Post("/secret", newSecret)
     })
-    router.Get("/secret/{id}", func(w http.ResponseWriter, r *http.Request) {
-        id := chi.URLParam(r, "id")
-        secretId, err := uuid.Parse(id)
-        if err != nil {
-            http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-            return
-        }
-        retrievedSecret, ok := SecretStore[secretId]
-        if ok && retrievedSecret.Views > 0 {
-            if !retrievedSecret.ClickThrough {
-                // Clickthrough not enabled, show secret immediately
-                retrievedSecret.Views--
-                SecretStore[secretId] = retrievedSecret
-                tmpl.Execute(w, retrievedSecret)
-            } else {
-                // Clickthrough enabled, return page with button to retrieve (additional request)
-                data := struct {
-                    SecretId string
-                } {
-                    SecretId: id,
-                }
-                clickthroughPage.Execute(w, data)
-            }
-        } else {
-            http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-            return
-        }
-    })
-    router.Post("/secret/{id}", func(w http.ResponseWriter, r *http.Request) {
-        id := chi.URLParam(r, "id")
-        secretId, err := uuid.Parse(id)
-        if err != nil {
-            http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-            return
-        }
-        retrievedSecret, ok := SecretStore[secretId]
-        if ok && retrievedSecret.Views > 0 {
-            retrievedSecret.Views--
-            SecretStore[secretId] = retrievedSecret
-            tmpl.Execute(w, retrievedSecret)
-        }
-    })
+    router.Get("/secret/{id}", getSecret)
+    router.Post("/secret/{id}", clickthroughRetrieveSecret)
 
     http.ListenAndServe(":8080", router)
 }
+
+func getSecret(w http.ResponseWriter, r *http.Request) {
+    id := chi.URLParam(r, "id")
+    secretId, err := uuid.Parse(id)
+    if err != nil {
+        http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+        return
+    }
+    retrievedSecret, ok := SecretStore[secretId]
+    if ok && retrievedSecret.Views > 0 {
+        if !retrievedSecret.ClickThrough {
+            // Clickthrough not enabled, show secret immediately
+            retrievedSecret.Views--
+            SecretStore[secretId] = retrievedSecret
+            tmpl.Execute(w, retrievedSecret)
+        } else {
+            // Clickthrough enabled, return page with button to retrieve (additional request)
+            data := struct {
+                SecretId string
+            } {
+                SecretId: id,
+            }
+            clickthroughPage.Execute(w, data)
+        }
+    } else {
+        http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+        return
+    }
+}
+
+func clickthroughRetrieveSecret(w http.ResponseWriter, r *http.Request) {
+    id := chi.URLParam(r, "id")
+    secretId, err := uuid.Parse(id)
+    if err != nil {
+        http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+        return
+    }
+    retrievedSecret, ok := SecretStore[secretId]
+    if ok && retrievedSecret.Views > 0 {
+        retrievedSecret.Views--
+        SecretStore[secretId] = retrievedSecret
+        tmpl.Execute(w, retrievedSecret)
+    }
+}
+
+func newSecret(w http.ResponseWriter, r *http.Request) {
+    payload := &Secret{}
+    d := json.NewDecoder(r.Body)
+    d.DisallowUnknownFields() // catch unwanted fields
+
+    err := d.Decode(&payload)
+    if err != nil {
+        // bad JSON or unrecognized json field
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    id := uuid.New()
+    if payload.Views < 1 {
+        payload.Views = 1
+    }
+    SecretStore[id] = *payload
+
+    w.Write([]byte(fmt.Sprintf("%v", id)))
+}
+
