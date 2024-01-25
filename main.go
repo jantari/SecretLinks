d79b512e5799ff9e14b04fa0fbe461e1b3b983e6
jantari <jantari@outlook.de>
2024-01-25 20:38:16 +0100

    fix golint complaints and remove unused CSS

--- main.go
+++ main.go
@@ -11,19 +11,22 @@ import (
     "github.com/google/uuid"
 )
 
-type Secret struct {
+type secret struct {
     Secret       string `json:"secret"`
     Views        int    `json:"views"`
     ClickThrough bool   `json:"click"`
 }
 
-var SecretStore = make(map[uuid.UUID]Secret)
+// This variable is overwritten at compile/link time using -ldflags
+var version = "development-build"
+
+var secretStore = make(map[uuid.UUID]secret)
 
 var tmpl = template.Must(template.ParseFiles("view.html"))
 var clickthroughPage = template.Must(template.ParseFiles("reveal.html"))
 
 func main() {
-    fmt.Println("SecretLinks")
+    fmt.Printf("SecretLinks %v\n", version)
 
     router := chi.NewRouter()
     router.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -40,24 +43,24 @@ func main() {
 
 func getSecret(w http.ResponseWriter, r *http.Request) {
     id := chi.URLParam(r, "id")
-    secretId, err := uuid.Parse(id)
+    secretID, err := uuid.Parse(id)
     if err != nil {
         http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
         return
     }
-    retrievedSecret, ok := SecretStore[secretId]
+    retrievedSecret, ok := secretStore[secretID]
     if ok && retrievedSecret.Views > 0 {
         if !retrievedSecret.ClickThrough {
             // Clickthrough not enabled, show secret immediately
             retrievedSecret.Views--
-            SecretStore[secretId] = retrievedSecret
+            secretStore[secretID] = retrievedSecret
             tmpl.Execute(w, retrievedSecret)
         } else {
             // Clickthrough enabled, return page with button to retrieve (additional request)
             data := struct {
-                SecretId string
+                SecretID string
             } {
-                SecretId: id,
+                SecretID: id,
             }
             clickthroughPage.Execute(w, data)
         }
@@ -69,21 +72,21 @@ func getSecret(w http.ResponseWriter, r *http.Request) {
 
 func clickthroughRetrieveSecret(w http.ResponseWriter, r *http.Request) {
     id := chi.URLParam(r, "id")
-    secretId, err := uuid.Parse(id)
+    secretID, err := uuid.Parse(id)
     if err != nil {
         http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
         return
     }
-    retrievedSecret, ok := SecretStore[secretId]
+    retrievedSecret, ok := secretStore[secretID]
     if ok && retrievedSecret.Views > 0 {
         retrievedSecret.Views--
-        SecretStore[secretId] = retrievedSecret
+        secretStore[secretID] = retrievedSecret
         tmpl.Execute(w, retrievedSecret)
     }
 }
 
 func newSecret(w http.ResponseWriter, r *http.Request) {
-    payload := &Secret{}
+    payload := &secret{}
     d := json.NewDecoder(r.Body)
     d.DisallowUnknownFields() // catch unwanted fields
 
@@ -98,7 +101,7 @@ func newSecret(w http.ResponseWriter, r *http.Request) {
     if payload.Views < 1 {
         payload.Views = 1
     }
-    SecretStore[id] = *payload
+    secretStore[id] = *payload
 
     w.Write([]byte(fmt.Sprintf("%v", id)))
 }
