c8881af07125d1bde13fe22d10e5496ae033f00a
jantari <jantari@outlook.de>
2024-01-29 22:51:40 +0100

    persistent secret storage using Bolt DB (bbolt)

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.21.5
 require (
 	github.com/go-chi/chi/v5 v5.0.11
 	github.com/google/uuid v1.5.0
+	github.com/peterbourgon/ff/v3 v3.4.0
+	go.etcd.io/bbolt v1.3.8
 )
 
-require github.com/peterbourgon/ff/v3 v3.4.0 // indirect
+require golang.org/x/sys v0.8.0 // indirect
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "os"
     "flag"
+    "time"
     "embed"
     "errors"
 
@@ -18,19 +19,24 @@ import (
     // Configuration
     "github.com/peterbourgon/ff/v3"
 
+    bolt "go.etcd.io/bbolt"
+
     "secretlinks/cryptopasta"
 )
 
-type secret struct {
+// API payload of incoming request (create new secret)
+type payloadSecret struct {
     Secret       string `json:"secret"`
     Views        int    `json:"views"`
     ClickThrough bool   `json:"click"`
 }
 
+// Data stored in persistent storage (secret encrypted)
 type storedSecret struct {
-    Secret       []byte
-    Views        int
-    ClickThrough bool
+    Secret       []byte `json:"secret"`
+    Views        int    `json:"views"`
+    ClickThrough bool   `json:"click"`
+    CreationDate string `json:"created"`
 }
 
 func (s storedSecret) decryptSecret(key [32]byte) (string, error) {
@@ -46,7 +52,7 @@ func (s storedSecret) decryptSecret(key [32]byte) (string, error) {
 // This variable is overwritten at compile/link time using -ldflags
 var version = "development-build"
 
-var secretStore = make(map[uuid.UUID]storedSecret)
+var db *bolt.DB
 
 //go:embed templates/*
 var embeddedTemplates embed.FS
@@ -58,9 +64,10 @@ func main() {
     fs := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
     var versionFlagPtr = fs.Bool("version", false, "Print the version information and exit")
     var listenAddrPtr  = fs.String("listen", "localhost:8080", "The address and port for the webserver to listen on")
+    var dbfilePtr      = fs.String("dbfile", "./store.db", "Path to the Bolt database file to store secrets")
 
     // Ingest configuration flags.
-    // Commandline arguments > Environment variables > config file
+    // Commandline arguments > Environment variables
     err := ff.Parse(
         fs,
         os.Args[1:],
@@ -84,6 +91,22 @@ func main() {
 
     viewPage = template.Must(template.ParseFS(embeddedTemplates, "templates/view.html"))
     clickthroughPage = template.Must(template.ParseFS(embeddedTemplates, "templates/reveal.html"))
+
+    // DB setup
+    db, err = bolt.Open(*dbfilePtr, 0600, &bolt.Options{Timeout: 10 * time.Second})
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    defer db.Close()
+
+    err = db.Update(func(tx *bolt.Tx) error {
+        _, err = tx.CreateBucketIfNotExists([]byte("Secrets"))
+        if err != nil {
+            return fmt.Errorf("error creating bucket: %s", err)
+        }
+        return nil
+    })
 
     router := chi.NewRouter()
     router.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -116,8 +139,9 @@ func getSecret(w http.ResponseWriter, r *http.Request) {
         http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
         return
     }
-    retrievedSecret, ok := secretStore[secretID]
-    if ok && retrievedSecret.Views > 0 {
+    retrievedSecret, err := getSecretFromDatabase(secretID)
+
+    if err == nil && retrievedSecret.Views > 0 {
         decryptedSecret, err := retrievedSecret.decryptSecret([32]byte(keyBytes))
         if err != nil {
             // Return 404 on decryption failures to not give away the requester
@@ -130,7 +154,15 @@ func getSecret(w http.ResponseWriter, r *http.Request) {
         if !retrievedSecret.ClickThrough {
             // Clickthrough not enabled, show secret immediately
             retrievedSecret.Views--
-            secretStore[secretID] = retrievedSecret
+            if retrievedSecret.Views < 1 {
+                err = deleteSecretFromDatabase(secretID)
+            } else {
+                err = storeSecretInDatabase(secretID, retrievedSecret, true)
+            }
+            if err != nil {
+                http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+                return
+            }
 
             viewPage.Execute(w, struct{
                 Secret string
@@ -163,8 +195,8 @@ func clickthroughRetrieveSecret(w http.ResponseWriter, r *http.Request) {
         http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
         return
     }
-    retrievedSecret, ok := secretStore[secretID]
-    if ok && retrievedSecret.Views > 0 {
+    retrievedSecret, err := getSecretFromDatabase(secretID)
+    if err == nil && retrievedSecret.Views > 0 {
         decryptedSecret, err := retrievedSecret.decryptSecret([32]byte(keyBytes))
         if err != nil {
             // Return 404 on decryption failures to not give away the requester
@@ -174,7 +206,16 @@ func clickthroughRetrieveSecret(w http.ResponseWriter, r *http.Request) {
             return
         }
         retrievedSecret.Views--
-        secretStore[secretID] = retrievedSecret
+        if retrievedSecret.Views < 1 {
+            err = deleteSecretFromDatabase(secretID)
+        } else {
+            err = storeSecretInDatabase(secretID, retrievedSecret, true)
+        }
+        if err != nil {
+            http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+            return
+        }
+
         viewPage.Execute(w, struct{
             Secret string
             Views int
@@ -189,7 +230,7 @@ func clickthroughRetrieveSecret(w http.ResponseWriter, r *http.Request) {
 }
 
 func newSecret(w http.ResponseWriter, r *http.Request) {
-    payload := &secret{}
+    payload := &payloadSecret{}
     d := json.NewDecoder(r.Body)
     d.DisallowUnknownFields() // catch unwanted fields
 
@@ -213,12 +254,66 @@ func newSecret(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    secretStore[id] = storedSecret{
+    data := storedSecret{
         Secret: encryptedSecret,
         Views: payload.Views,
         ClickThrough: payload.ClickThrough,
+        CreationDate: time.Now().UTC().Format(time.RFC3339),
+    }
+    err = storeSecretInDatabase(id, data, false)
+    if err != nil {
+        fmt.Printf("error storing secret in DB: %v\n", err)
+        http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+        return
     }
 
     w.Write([]byte(fmt.Sprintf("secret/%v/%v\n", id, base64.URLEncoding.EncodeToString(key[:]))))
 }
 
+func getSecretFromDatabase(key uuid.UUID) (storedSecret, error) {
+    var retrievedSecret storedSecret
+    err := db.View(func(tx *bolt.Tx) error {
+        b := tx.Bucket([]byte("Secrets"))
+        v := b.Get(key[:])
+        if v == nil {
+            return nil
+        }
+        err := json.Unmarshal(v, &retrievedSecret)
+        if err != nil {
+            return err
+        }
+        return nil
+    })
+    if err != nil {
+        fmt.Printf("error getting secret from DB: %v\n", err)
+    }
+    return retrievedSecret, nil
+}
+
+func storeSecretInDatabase(key uuid.UUID, value storedSecret, updateIfExists bool) error {
+    jsonData, err := json.Marshal(value)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    err = db.Update(func(tx *bolt.Tx) error {
+        b := tx.Bucket([]byte("Secrets"))
+        // Abort if the key already exists and update isn't set
+        if updateIfExists != true && b.Get(key[:]) != nil {
+            return errors.New("key aready exists")
+        }
+        err := b.Put(key[:], []byte(jsonData))
+        return err
+    })
+
+    return nil
+}
+
+func deleteSecretFromDatabase(key uuid.UUID) error {
+    err := db.Update(func (tx *bolt.Tx) error {
+        b := tx.Bucket([]byte("Secrets"))
+        err := b.Delete(key[:])
+        return err
+    })
+    return err
+}
